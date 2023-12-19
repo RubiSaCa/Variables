6e3542f63af0d8f3c85422e71d93486847eb5842
Rubí <rubi_salcedocas@outlook.es>
2023-12-19 01:46:57 -0700

    Se agrega el proyecto

---
+++ src/rubi/com/main/Variables.java
@@ -0,0 +1,15 @@
+package rubi.com.main;
+
+public abstract class Variables {
+    public static void main (String[] args) {
+       //Declarando una variable
+        int speed;
+        //Asignamos un valor a una vaiable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Rubi";
+        System.out.println(employeeName);
+    }
+}
